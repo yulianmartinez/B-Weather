9c0ee4285745bbf1091ae90935c0b55f3b5fb3bf
Yulian Martinez <yulian.personal@gmail.com>
2026-05-29 05:23:08 -0500

    Se agrega Android Lint y se ajustan advertencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ymd.bweather"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.bold.bweather"
--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 android {
     namespace = "com.bold.core.network"
-    compileSdk = 36
+    compileSdk = 37
     defaultConfig {
         minSdk = 26
     }
